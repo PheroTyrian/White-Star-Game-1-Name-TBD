a9c196f685c30e599a7ab7c830f08e26381e5288
northy97 <46992945+northy97@users.noreply.github.com>
2019-03-20 13:40:22 +0000

    EntityMovementWorkingTested

--- Code/SourceCode/HAPI_APP/BattleSystem.cpp
+++ Code/SourceCode/HAPI_APP/BattleSystem.cpp
@@ -12,38 +12,83 @@ BattleSystem::BattleSystem() :
 	UIWind()
 {
 	Entity* testShip = new Entity("Data\\mouseCrossHair.xml");
-	m_entities.push_back(
-		std::pair<Entity*, std::pair<int, int> >(testShip, std::pair<int, int>(4, 4)));
+	Entity* testShip2 = new Entity("Data\\thingy.xml");
+	m_entities.push_back(std::pair<Entity*, std::pair<int, int> >(testShip, std::pair<int, int>(4, 4)));
+	m_entities.push_back(std::pair<Entity*, std::pair<int, int> >(testShip2, std::pair<int, int>(5, 5)));
+
+
+	for (auto i : m_entities)
+	{
+		m_map.insertEntity(i.first, i.second);
+	}
+	
+	
 }
+
 
 BattleSystem::~BattleSystem()
 {
 	for (auto it : m_entities)
 	{
-		delete it.first;
+		//delete it.first;
 	}
 	m_entities.clear();
 }
 
 void BattleSystem::update()
 {
+	
 	//temp map set entity to tile 
-	m_map.insertEntity(m_entities[0].first,m_entities[0].second);
-
+	{
+	std::pair<float,float> tempTileLocation ;
+	
 	while (HAPI_Sprites.Update())// work on adding entity tile switching is temp 
 	{
 		SCREEN_SURFACE->Clear();
-
+		
 		m_map.drawMap();
-		m_entities[0].first->render();
+		
 		UIWind.Update();
-
+		
+		// test
 		for (int x = 0; x < m_map.getMap()->size(); x++) // temp these 2 vectors not gonna be public had to get test working 
 		{
-			//need to talk to tristan about entitys
 			UIWind.HandleCollision(*UIWind.storage[UIWind.storage.size() - 1], *m_map.getMap()->data()[x].m_sprite);
+			tempTileLocation = std::pair<float, float>(m_map.getMap()->data()[x].m_sprite->GetTransformComp().GetPosition().x, m_map.getMap()->data()[x].m_sprite->GetTransformComp().GetPosition().y);
+		
+			if (tempTileLocation == UIWind.tilePos)
+			{
+				
+				coord = m_map.getMap()->data()[x].m_tileCoordinate;
+			}
+			
+			for (int x = 0; x < m_entities.size(); x++)
+			{
+				if (m_entities[x].second == coord)
+				{
+					entityPositionInVector = x;
+				}
+			}
+			
+			if (m_map.moveEntity(std::pair<int, int>(m_entities[entityPositionInVector].second), coord))
+			{
+				m_entities[entityPositionInVector].second = coord;
+			}
+
+			if (m_map.getMap()->data()[x].m_entityOnTile != nullptr)
+			{
+				m_map.getMap()->data()[x].m_entityOnTile->getSprite().GetTransformComp().SetPosition({ m_map.getMap()->data()[x].m_sprite->GetTransformComp().GetPosition().x + 30, m_map.getMap()->data()[x].m_sprite->GetTransformComp().GetPosition().y + 40 });
+				//m_map.getMap()->data()[x].m_sprite->AdvanceToNextFrame();
+				
+				m_map.getMap()->data()[x].m_entityOnTile->render();
+	
+			}
+			
 		}
 	}
+
+}
+	//m_entities[0].first->getSprite().GetTransformComp().SetPosition({ (float)tempTileLocation.first + 30,(float)tempTileLocation.second + 40 });
 	running = false;
 }
 
--- Code/SourceCode/HAPI_APP/BattleSystem.h
+++ Code/SourceCode/HAPI_APP/BattleSystem.h
@@ -7,6 +7,7 @@
 #include "Map.h"
 #include "UIClass.h"
 
+
 using namespace HAPISPACE;
 
 class BattleSystem
@@ -15,9 +16,14 @@ private:
 	void update();
 
 	std::vector<std::pair<Entity*, std::pair<int, int>>> m_entities;
+	std::pair<int, int>oldPos;
+	std::pair<int, int>newPos;
 	bool running;
 	Map m_map;
 	UIWindowTest UIWind;
+	std::pair<int, int>coord;
+	bool select{ true };// temp boll used as trigger for the second selection
+	int entityPositionInVector;
 	
 public:
 	BattleSystem();
